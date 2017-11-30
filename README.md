06c7d3286c8c2034dfd84ab6d4d17236cfe1104f
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2017-11-30 11:12:45 -0500

    Basic menu setup.

---
+++ README.md
@@ -0,0 +1,1 @@
+# SystemLevel-Text_Edit
---
+++ simple_text.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+echo -e "1: Load a text file \t\t\t 2: Display the text file
+3: Show the frequency of a string \t 4: Find a string
+5: Replace a string \t\t\t 6: Insert a string
+7: Delete a string \t\t\t 8: Exit"
+
+load () {
+    # Ask for input for text file name
+}
+
+display () {
+    # Print contents of file loaded
+}
+
+count_string () {
+    # Prompt for string input
+    # Count frequency of s in file
+}
+
+replace_string () {
+    # Prompt for string to replace
+    # Prompt for new string
+}
+
+insert_string () {
+    # Prompt for existing string input, the new one will be inserted after
+    # Prompt for new string
+    # Insert new string after existing string
+}
+
+delete_string () {
+    # Prompt for string and then delete it.
+}
