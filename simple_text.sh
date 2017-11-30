10e63db8bd77cce74277f3fe4963ac783791c593
crumjo <crumjo@mail.gvsu.edu>
2017-11-30 14:10:53 -0500

    Basic layout and exit set up. Load is outlined.

--- simple_text.sh
+++ simple_text.sh
@@ -1,38 +1,70 @@
 #! /bin/bash
 
+load () {
+    echo -n "Enter a file to load: "
+    read file
+
+    if [ ! -f $file ]
+    then
+        printf "'$file' does not exist.\n"
+    else
+        printf "'$file' has been loaded.\n"
+    fi
+}
+
+display_file () {
+    echo "Print contents of file loaded."
+}
+
+count_string () {
+    echo "Prompt for string input."
+    echo "Count frequency of s in file."
+}
+
+replace_string () {
+    echo "Prompt for string to replace."
+    echo "Prompt for new string."
+}
+
+insert_string () {
+    echo "Prompt for existing string input, the new one will be inserted after."
+    echo "Prompt for new string."
+    echo "Insert new string after existing string."
+}
+
+delete_string () {
+    echo "Prompt for string and then delete it."
+}
+
 echo -e "1: Load a text file \t\t\t 2: Display the text file
 3: Show the frequency of a string \t 4: Find a string
 5: Replace a string \t\t\t 6: Insert a string
 7: Delete a string \t\t\t 8: Exit"
 
-load () {
-    # Ask for input for text file name
-}
+load_check=0
+while [ $load_check -ne 1 ]
+do
+    echo -n "Enter a 1 to load a text file: "
+    read load_choice
+    echo "LOAD CHOICE: $load_choice"
 
-display () {
-    # Print contents of file loaded
-}
+    if [ $load_choice -eq 1 ]
+    then
+        printf "Good choice!\n"
+        let load_check=$load_check+1
+    else
+        printf "Bad choice...\n"
+    fi
+done
 
-count_string () {
-    # Prompt for string input
-    # Count frequency of s in file
-}
+while [ 1 ]
+do
+    echo -n "Enter a command: "
+    read cmd
 
-replace_string () {
-    # Prompt for string to replace
-    # Prompt for new string
-}
+    if [ $cmd -eq 8 ]
+    then
+        exit 1
+    fi
 
-insert_string () {
-    # Prompt for existing string input, the new one will be inserted after
-    # Prompt for new string
-    # Insert new string after existing string
-}
-
-delete_string () {
-    # Prompt for string and then delete it.
-}
-
-test_func () {
-	# foo
-}
+done
